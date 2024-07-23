43e63d1dfe76bf32edd165d0e51aab66d35d32e0
Syodai88 <syodi0808@gmail.com>
2024-07-24 00:07:25 +0900

    ログインページを作成

---
+++ kit-next/src/app/login/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Form from '../../components/common/Form'
+import { motion } from 'framer-motion'
+
+const Login: React.FC = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.3 }}
+      className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-semibold text-gray-800 mb-6">ログイン</h2>
+        <Form type="login" />
+      </div>
+    </motion.div>
+  )
+}
+
+export default Login
