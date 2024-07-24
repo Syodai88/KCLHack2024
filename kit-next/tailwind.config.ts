289dbabd0687383d25f87da276766c207688fa9e
Syodai88 <syodi0808@gmail.com>
2024-07-24 11:41:23 +0900

    アプリのメインカラーの設定

--- kit-next/tailwind.config.ts
+++ kit-next/tailwind.config.ts
@@ -2,19 +2,30 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary': '#64BCFC',
+        'primary-dark': '#4A9FE0',
+        'secondary': '#68C5F3',
+        'secondary-dark': '#4FA8D6',
+        'accent': '#454545',
+        'background': '#DFDFDF',
+        'warning': '#FFC107', // 警告色
+        'success': '#28A745', // 成功色
+        'error': '#DC3545',   // エラー色
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 };
 export default config;
