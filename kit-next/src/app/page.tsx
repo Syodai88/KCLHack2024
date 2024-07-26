fc39d2b8296c3db0c442ed54232144c1dbe8a0ba
Syodai88 <syodi0808@gmail.com>
2024-07-27 00:14:00 +0900

    Verifyページから遷移すると文字が巨大化するバグが発生 おそらくcssとMUIの競合なので標準のタグに変更

--- kit-next/src/app/page.tsx
+++ kit-next/src/app/page.tsx
@@ -1,6 +1,6 @@
+// Home.tsx
 "use client";
 import { motion } from 'framer-motion';
-import { Container, Typography } from '@mui/material';
 
 const Home: React.FC = () => {
   return (
@@ -10,14 +10,14 @@ const Home: React.FC = () => {
       transition={{ duration: 0.5 }}
       className="flex flex-col items-center justify-center min-h-[calc(100vh-8rem)]"
     >
-      <Container maxWidth="md" className="text-center">
-        <Typography variant="h1" component="h1" className="text-4xl md:text-5xl font-bold text-white mb-8">
-          あなたの未来へのジャンプスタート
-        </Typography>
-        <Typography variant="body1" className="text-xl text-white mb-12 max-w-2xl mx-auto">
-          就活のプロセスをスムーズに。情報、ツール、サポート、すべてここにあります。
-        </Typography>
-      </Container>
+      <div className="container max-w-screen-md text-center px-4">
+        <h1 className="text-5xl md:text-5xl font-bold text-white mb-8">
+          あなたの未来へのスタート！！
+        </h1>
+        <p className="text-2xl text-white mb-12 max-w-2xl mx-auto">
+          九工大生に寄り添った情報がここに、、！
+        </p>
+      </div>
     </motion.div>
   );
 }
