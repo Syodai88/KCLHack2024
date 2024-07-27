1943a5856202e34c51868c9ab314cc39de3fd9b1
Syodai88 <syodi0808@gmail.com>
2024-07-27 18:58:59 +0900

    各関数内にエラー処理を追加 ログインで認証していないメールの場合は再度メールを送信してログインさせないように変更 登録後に自動的にページ遷移されるように変更

--- kit-next/src/context/AuthContext.tsx
+++ kit-next/src/context/AuthContext.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { useRouter } from 'next/navigation';
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, User } from 'firebase/auth';
+import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, User, sendEmailVerification, applyActionCode } from 'firebase/auth';
 import { auth } from './../plugins/firebase';
+import firebase from 'firebase/compat/app';
 
 interface AuthContextProps {
   user: User | null;
@@ -9,6 +11,7 @@ interface AuthContextProps {
   login: (email: string, password: string) => Promise<void>;
   register: (email: string, password: string) => Promise<void>;
   logout: () => void;
+  verifyEmail: (oobCode: string) => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
@@ -16,6 +19,7 @@ const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -26,11 +30,75 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   const login = async (email: string, password: string) => {
-    await signInWithEmailAndPassword(auth, email, password);
+    try{
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const isNotVerified = !userCredential.user.emailVerified;
+      if (isNotVerified) {
+        await reSendVerifyMail(userCredential.user);
+        await logout();
+        console.error('Your email address has not been confirmed. A confirmation email has been sent again.')
+      }
+    } catch (error: any){
+        switch (error.code){
+          case 'auth/invalid-email':
+            console.error('Invalid email.');
+            break;
+          case 'auth/user-not-found':
+            console.error('Invalid email or user not found.');
+            break;
+          case 'auth/wrong-password':
+            console.error('Incorrect password.');
+            break;
+          case 'auth/too-many-requests':
+            console.error('Too many requests. Please try again later.');
+            break;
+          default:
+            console.error('Failed to log in:', error.message);
+        }
+        return;
+    }
+
   };
 
+  const reSendVerifyMail = async (user: User) => {
+    try {
+      if (user){
+        await sendEmailVerification(user);
+      }
+      return;
+    } catch (error: any){
+        switch (error.code){
+          case 'auth/too-many-requests':
+            console.error('Too many requests. Please try again later.');
+            break;
+          default:
+            console.error('Failed to send verification email:', error.message);
+        }
+        return;
+    }
+  }
+
   const register = async (email: string, password: string) => {
-    await createUserWithEmailAndPassword(auth, email, password);
+    try{
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      await sendEmailVerification(userCredential.user);
+      router.push('/verify');
+    } catch (error: any){
+        switch (error.code){
+          case 'auth/email-already-in-use':
+            console.error('The email address is already in use by another account.');
+            break;
+          case 'auth/invalid-email':
+            console.error('Invalid email.');
+            break;
+          case 'auth/weak-password':
+            console.error('The password must be 6 characters long or more.');
+            break;
+          default:
+            console.error('Failed to register:', error.message);
+        }
+        return;
+    }
   };
 
   const logout = async () => {
@@ -38,8 +106,26 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setUser(null);
   };
 
+  const verifyEmail = async (oobCode: string) => {
+    try {
+      await applyActionCode(auth, oobCode);
+    } catch(error: any){
+      switch (error.code){
+        case 'auth/invalid-action-code':
+          console.error('The action code is invalid.');
+          break;
+        case 'auth/expired-action-code':
+          console.error('The action code has expired.');
+          break;
+        default:
+          console.error('Failed to verify email:', error.message);
+      }
+      return;
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user, loading, login, register, logout }}>
+    <AuthContext.Provider value={{ user, loading, login, register, logout, verifyEmail }}>
       {children}
     </AuthContext.Provider>
   );
