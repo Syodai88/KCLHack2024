59ecc97a051c4fbc32d3a204802f28f041b30222
Syodai88 <syodi0808@gmail.com>
2024-07-27 23:11:18 +0900

    不要なコードを削除(モバイル) authフォルダ作成に伴いLinkの遷移先を修正

--- kit-next/src/components/common/Navbar.tsx
+++ kit-next/src/components/common/Navbar.tsx
@@ -1,11 +1,9 @@
 "use client";
 import Link from 'next/link';
 import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 
 
 const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);  // ログイン状態を管理する状態
 
   const handleLogout = () => {
@@ -45,55 +43,13 @@ const Navbar: React.FC = () => {
               </>
             ) : (
               <>
-                <Link href="/login" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-primary hover:text-white transition duration-300">ログイン</Link>
-                <Link href="/register" className="py-2 px-2 font-medium text-white bg-primary rounded hover:bg-primary-dark transition duration-300">新規登録</Link>
+                <Link href="/auth/login" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-primary hover:text-white transition duration-300">ログイン</Link>
+                <Link href="/auth/register" className="py-2 px-2 font-medium text-white bg-primary rounded hover:bg-primary-dark transition duration-300">新規登録</Link>
               </>
             )}
-          </div>
-          <div className="md:hidden flex items-center">
-            <button className="outline-none mobile-menu-button" onClick={() => setIsOpen(!isOpen)}>
-              <svg className="w-6 h-6 text-gray-500 hover:text-primary"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </button>
           </div>
         </div>
       </div>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div 
-            className="md:hidden"
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: "auto" }}
-            exit={{ opacity: 0, height: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            {isLoggedIn ? (
-              <>
-                <span className="block py-2 px-4 text-sm">日付: {new Date().toLocaleDateString()}</span>
-                <button 
-                  onClick={handleLogout} 
-                  className="block py-2 px-4 text-sm text-white bg-primary rounded hover:bg-primary-dark transition duration-300"
-                >
-                  ログアウト
-                </button>
-              </>
-            ) : (
-              <>
-                <Link href="/login" className="block py-2 px-4 text-sm hover:bg-primary hover:text-white transition duration-300">ログイン</Link>
-                <Link href="/register" className="block py-2 px-4 text-sm hover:bg-primary hover:text-white transition duration-300">新規登録</Link>
-              </>
-            )}
-          </motion.div>
-        )}
-      </AnimatePresence>
     </nav>
   )
 }
