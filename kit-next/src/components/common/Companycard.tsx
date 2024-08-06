4a871936c78baef824330b4aa2707da147c9a497
snowYuKi1420 <yk95.aio@gmail.com>
2024-08-06 14:16:12 +0900

    Update Companycard.tsx

--- kit-next/src/components/common/Companycard.tsx
+++ kit-next/src/components/common/Companycard.tsx
@@ -1,35 +1,48 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { Card, CardContent, CardMedia, Typography, CardActions, Button, Box } from '@mui/material';
 
-const Companycard: React.FC = () => {
+interface CompanyCardProps {
+  image: string;
+  name: string;
+  details: string;
+}
+
+const CompanyCard: React.FC<CompanyCardProps> = ({ image, name, details }) => {
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
+    <Card sx={{ display: 'flex', marginBottom: 2 }}>
+      <CardMedia
+        component="img"
+        sx={{ width: 160 }}
+        image={image}
+        alt={name}
+      />
+      <CardContent sx={{ flex: '1 0 auto' }}>
+        <Typography component="div" variant="h5">
+          {name}
         </Typography>
-        <Typography variant="h5" component="div">
-          benevolent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography variant="subtitle1" color="text.secondary" component="div">
+          {details}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
+      <CardActions sx={{ padding: 2 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+          <Button
+            size="small"
+            variant="outlined"
+            sx={{ marginRight: 1 }}
+          >
+            Button 1
+          </Button>
+          <Button
+            size="small"
+            variant="outlined"
+          >
+            Button 2
+          </Button>
+        </Box>
       </CardActions>
     </Card>
   );
-}
+};
 
-export default Companycard;
+export default CompanyCard;
