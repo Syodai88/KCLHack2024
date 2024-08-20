3032ad9b28cd855a1b79f0c4cc8b0dbf75ad707c
Syodai88 <syodi0808@gmail.com>
2024-08-20 17:34:57 +0900

    テストページに分割表示を設定

--- kit-next/src/app/test/page.tsx
+++ kit-next/src/app/test/page.tsx
@@ -3,6 +3,8 @@
 
 import { useState, useEffect } from 'react';
 import SearchForm from '../../components/search/searchForm';
+import SplitPage from '@/components/common/SplitPage';
+
 
 const Home: React.FC = () => {
   const [results, setResults] = useState(null);
@@ -24,15 +26,17 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="space-y-6">
-      <SearchForm onSearch={searchCompany} />
-      {results && (
-        <div className="mt-6 bg-white p-6 rounded-lg shadow-md w-full">
-          <h2 className="text-2xl font-semibold text-accent mb-4">Search Results:</h2>
-          <pre className="bg-gray-100 p-4 rounded-md text-gray-700">{JSON.stringify(results, null, 2)}</pre>
-        </div>
-      )}
-    </div>
+    <SplitPage sidebar={<p className="text-black">Sidebar Content</p>}>
+      <div className="space-y-6">
+        <SearchForm onSearch={searchCompany} />
+        {results && (
+          <div className="mt-6 bg-white p-6 rounded-lg shadow-md w-full">
+            <h2 className="text-2xl font-semibold text-accent mb-4">Search Results:</h2>
+            <pre className="bg-gray-100 p-4 rounded-md text-gray-700">{JSON.stringify(results, null, 2)}</pre>
+          </div>
+        )}
+      </div>
+    </SplitPage>
   );
 }
 
