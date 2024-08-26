08841ddb346246eeb68ad123156ac5838a3d28ff
natsuki-818 <natsuki.818_@outlook.jp>
2024-08-26 21:14:40 +0900

    サイドバーのレイアウト変更

--- kit-next/src/app/layout.tsx
+++ kit-next/src/app/layout.tsx
@@ -2,9 +2,8 @@ import './globals.css'
 import { Metadata } from 'next'
 import Navbar from './../components/common/Navbar'
 import Footer from './../components/common/Footer'
-import SplitPage from '@/components/common/SplitPage'
 import { useAuth, AuthProvider } from './../context/AuthContext'
-import Sidebar from './../components/common/Sidebar'
+
 
 export const metadata: Metadata = {
   title: '就活支援アプリ | 大学生の未来をサポート',
@@ -17,11 +16,9 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <html lang="ja">
         <body className="bg-gradient-to-br from-primary to-secondary min-h-screen flex flex-col">
           <Navbar />
-          <SplitPage sidebar={<Sidebar />}>
           <main className="container mx-auto px-4 py-8 flex-grow">
             {children}
           </main>
-          </SplitPage>
           <Footer />
         </body>
       </html>
