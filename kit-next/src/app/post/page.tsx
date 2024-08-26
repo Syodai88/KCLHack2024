cc3b4a66d71318ba747908563ab2812e0f187ee3
snowYuKi1420 <yk95.aio@gmail.com>
2024-08-27 02:17:22 +0900

    Update page.tsx

--- kit-next/src/app/post/page.tsx
+++ kit-next/src/app/post/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '../../context/AuthContext';
+import { TextField, Box} from '@mui/material';
 
 const Home: React.FC = () => {
   const { user, loading } = useAuth();
@@ -17,11 +18,39 @@ const Home: React.FC = () => {
 //     return <div>Loading...</div>;
 //   }
 
-  return (
-    <div>
-      <h1>Welcome, {user?.email}</h1>
-      <p>User ID: {user?.uid}</p>
-    </div>
+return (
+    <Box 
+      sx={{
+        width: '80%', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        margin: '0 auto',
+      }}
+    >
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <TextField
+          fullWidth 
+          id="filled-basic" 
+          label="タイトル" 
+          variant="outlined" 
+          sx={{ width: '60%', backgroundColor: 'white' }}
+        />
+      </div>
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <TextField
+          fullWidth
+          id="filled-multiline-static"
+          label="本文"
+          multiline
+          rows={25}
+          defaultValue="Default Value"
+          variant="outlined"
+          sx={{ width: '80%', backgroundColor: 'white' }}
+        />
+      </div>
+    </Box>
   );
 };
 
