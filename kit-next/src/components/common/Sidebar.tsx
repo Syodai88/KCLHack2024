016a36273017a1b19a17e6bfb4be5f35cf4288f5
Syodai88 <syodi0808@gmail.com>
2024-09-08 15:57:37 +0900

    refactor: Rename company registration folder to RegisterCompany

--- kit-next/src/components/common/Sidebar.tsx
+++ kit-next/src/components/common/Sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar: React.FC = () => {
             </div>
             <Link href="/post-page" className="block text-blue-500 no-underline my-2">感想投稿ページ</Link>
             <Link href="/home" className="block text-blue-500 no-underline my-2">企業検索ページ</Link>
-            <Link href="/companyRegister" className="block text-blue-500 no-underline my-2">企業登録ページ</Link>
+            <Link href="/registerCompany" className="block text-blue-500 no-underline my-2">企業登録ページ</Link>
             <Link href="/details-page" className="block text-blue-500 no-underline my-2">感想詳細ページ</Link>
         </div>
     );
