222a71cad27f052b3377c24a6a0a86fa8222d7cf
Syodai88 <syodi0808@gmail.com>
2024-09-08 15:48:57 +0900

    fix: Correct href link addresses and update img to Image component

--- kit-next/src/components/common/Sidebar.tsx
+++ kit-next/src/components/common/Sidebar.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import Avatar from '@mui/material/Avatar';
+import Image from 'next/image';
 
 const Sidebar: React.FC = () => {
     const router = useRouter();
@@ -14,13 +15,13 @@ const Sidebar: React.FC = () => {
     return (
         <div className="p-5 bg-white h-full overflow-y-auto">
             <div className="mb-5 cursor-pointer" onClick={handleAvatarClick}>
-                <Avatar>
-                    <img src="/path-to-avatar-icon.png" alt="avatar" />
+                <Avatar sx={{ width: 80, height: 80 }}>
+                    <Image src="/path-to-avatar-icon.png" alt="avatar" width={40} height={40} />
                 </Avatar>
             </div>
             <Link href="/post-page" className="block text-blue-500 no-underline my-2">感想投稿ページ</Link>
-            <Link href="/search-page" className="block text-blue-500 no-underline my-2">企業検索ページ</Link>
-            <Link href="/register-page" className="block text-blue-500 no-underline my-2">企業登録ページ</Link>
+            <Link href="/home" className="block text-blue-500 no-underline my-2">企業検索ページ</Link>
+            <Link href="/companyRegister" className="block text-blue-500 no-underline my-2">企業登録ページ</Link>
             <Link href="/details-page" className="block text-blue-500 no-underline my-2">感想詳細ページ</Link>
         </div>
     );
