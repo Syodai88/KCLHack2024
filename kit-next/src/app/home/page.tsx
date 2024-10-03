fee8e89e3544c753cfc0c267bea735feb53ccbe4
Syodai88 <syodi0808@gmail.com>
2024-10-03 11:03:41 +0900

    上位３企業を表示するように変更

--- kit-next/src/app/home/page.tsx
+++ kit-next/src/app/home/page.tsx
@@ -6,18 +6,34 @@ import Companycard from '../../components/common/Companycard'
 import Sidebar from '@/components/common/Sidebar';
 import SplitPage from '@/components/common/SplitPage';
 import SearchForm from '@/components/registercompany/searchForm';
+interface Company  {
+  corporateNumber: string;
+  name: string;
+  location: string;
+  companyUrl: string;
+  businessSummary: string;
+  businessSummaryAi : string;
+  interestedCount: number;
+  updateDate: string;
+}
 
 const Content: React.FC = () => {
-  interface Company  {
-    corporate_number: string;
-    name: string;
-    location: string;
-    status: string;
-    update_date: string;
-  }
 
   const { user, loading } = useAuth();
   const router = useRouter();
+  const [topCompanies, setTopCompanies] = useState<Company[]>([]);
+
+  const fetchTopCompanies = async () => {
+    try {
+      const res = await fetch('/api/popularCompany');
+      const data = await res.json();
+      setTopCompanies(data);
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching top companies:', error);
+      setTopCompanies([]);
+    }
+  };
   
 
   useEffect(() => {
@@ -26,36 +42,38 @@ const Content: React.FC = () => {
     }
   }, [user, loading, router]);
 
+  useEffect(() => {
+    fetchTopCompanies(); // ページロード時に上位企業を取得
+  }, []);
+
   if (loading) {
     return <div>Loading...</div>;
   }
+  
 
   return (
     <div>
       <h1>Welcome, {user?.email}</h1>
       <p>User ID: {user?.uid}</p>
       <div>
-      <Companycard           
-        image='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg6WtGDvj1jszSP30qcBgkdNhSRkB4FHNGiN8s85mvGUDw-w2H3Hw-numR7W7tkWygsQ4mG-fLTBotRvV86eVJTdd473sryVzgrMx_Nxbs1IDuHQ0rNwWfbvoC6Zd1OFEpbMMBfE2YmN2I/s800/business_icon_big_company.png'
-        name='ネオテック・エンジニアリング'
-        details='最先端技術を駆使し、持続可能な未来を創造する総合エンジニアリング企業。イノベーションに重点を置く。'/>
-      </div>
-      <div>
-      <Companycard           
-        image='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg6WtGDvj1jszSP30qcBgkdNhSRkB4FHNGiN8s85mvGUDw-w2H3Hw-numR7W7tkWygsQ4mG-fLTBotRvV86eVJTdd473sryVzgrMx_Nxbs1IDuHQ0rNwWfbvoC6Zd1OFEpbMMBfE2YmN2I/s800/business_icon_big_company.png'
-        name='インフィニティ・ソリューションズ'
-        details='無限の可能性を引き出す技術革新企業。顧客のニーズに合わせたカスタマイズソリューションを提供。'/>
-      </div>
-      <div>
-      <Companycard           
-        image='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg6WtGDvj1jszSP30qcBgkdNhSRkB4FHNGiN8s85mvGUDw-w2H3Hw-numR7W7tkWygsQ4mG-fLTBotRvV86eVJTdd473sryVzgrMx_Nxbs1IDuHQ0rNwWfbvoC6Zd1OFEpbMMBfE2YmN2I/s800/business_icon_big_company.png'
-        name='テクノフュージョン・デザイン'
-        details='技術とデザインの融合で新しい価値を提供する企業。エンジニアリングとクリエイティブの力を結集。'/>
+      {topCompanies.length > 0 ? (
+            topCompanies.map((company) => (
+              <Companycard
+                key={company.corporateNumber}
+                image='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg6WtGDvj1jszSP30qcBgkdNhSRkB4FHNGiN8s85mvGUDw-w2H3Hw-numR7W7tkWygsQ4mG-fLTBotRvV86eVJTdd473sryVzgrMx_Nxbs1IDuHQ0rNwWfbvoC6Zd1OFEpbMMBfE2YmN2I/s800/business_icon_big_company.png'
+                name={company.name}
+                details={`所在地: ${company.location}\n業務概要: ${company.businessSummary}\n最終更新日: ${company.updateDate}\nLike Count: ${company.interestedCount}`}
+              />
+            ))
+          ) : (
+            <p>上位企業情報が見つかりませんでした。</p>
+          )}
       </div>
 
     </div>
   );
 };
+
 const Home: React.FC = () => {
   interface Company  {
     corporate_number: string;
