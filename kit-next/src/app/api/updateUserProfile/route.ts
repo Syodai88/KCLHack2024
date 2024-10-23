ab9b16856e5c0c2eba1ebd73d7b52dfb61f30436
Syodai88 <syodi0808@gmail.com>
2024-10-23 09:48:00 +0900

    ユーザーのプロフィールを保存するためのAPIの作成

---
+++ kit-next/src/app/api/updateUserProfile/route.ts
@@ -0,0 +1,31 @@
+// /pages/api/updateUserProfile.ts
+import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { userId, profile } = body;
+
+    if (!userId || !profile) {
+      return NextResponse.json({ error: 'userId and profile are required' }, { status: 400 });
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        name: profile.handleName,
+        year: profile.year,
+        department: profile.department,
+        other: profile.other,
+      },
+    });
+
+    return NextResponse.json(updatedUser, { status: 200 });
+  } catch (error) {
+    console.error('ユーザープロフィールの更新エラー:', error);
+    return NextResponse.json({ error: 'Failed to update user profile' }, { status: 500 });
+  }
+}
