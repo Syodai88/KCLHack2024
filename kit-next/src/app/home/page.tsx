5fd429ffac9587aec60e869782754ca275bb7bef
Syodai88 <syodi0808@gmail.com>
2024-11-04 17:35:34 +0900

    popularCompanyの時のみisPopularフラグを設定するように修正 それに合わせてinterfaceにもオプションのプロパティを追加 Searchformのplaceholderに文字列を設定

--- kit-next/src/app/home/page.tsx
+++ kit-next/src/app/home/page.tsx
@@ -30,16 +30,28 @@ const Content: React.FC<ContentProps> = ({ companies }) => {
   return (
     <div style={{ marginTop: '30px' }}>
       {companies.length > 0 ? (
-        companies.map((company : Company) => (
-          <Companycard
-            key={company.corporateNumber}
-            userId={user?.uid || ''}
-            company={company}
-            image='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg6WtGDvj1jszSP30qcBgkdNhSRkB4FHNGiN8s85mvGUDw-w2H3Hw-numR7W7tkWygsQ4mG-fLTBotRvV86eVJTdd473sryVzgrMx_Nxbs1IDuHQ0rNwWfbvoC6Zd1OFEpbMMBfE2YmN2I/s800/business_icon_big_company.png'
-          />
-        ))
+        companies.map((company: Company, index: number) => {
+          let image = '/Company.svg'; 
+          if (company.isPopular){
+              if (index === 0) {
+                image = '/Gold.svg';
+              }else if (index === 1) {
+                image = '/Silver.svg';
+              } else if (index === 2) {
+                image = '/Bronze.svg';
+              }
+          }
+          return (
+            <Companycard
+              key={company.corporateNumber}
+              userId={user?.uid || ''}
+              company={company}
+              image={image}
+            />
+          );
+        })
       ) : (
-        <Loading/>
+        <p>検索結果がありません</p>
       )}
     </div>
   );
@@ -48,11 +60,21 @@ const Content: React.FC<ContentProps> = ({ companies }) => {
 const Home: React.FC = () => {
   const [results, setResults] = useState<Company[]>([]);
 
+  //PopularCompanyにフラグを設定
+  const markPopularCompanies =(companies : Company[], popularCompanyIds : string[])=>{
+    return companies.map(company => ({
+      ...company,
+      isPopular : popularCompanyIds.includes(company.corporateNumber),
+    }));
+  };
+
   const fetchPopularCompanies = async () => {
     try {
       const res = await fetch('/api/popularCompany');
       const data = await res.json();
-      setResults(data);
+      const popularIds = data.map((company: Company) => company.corporateNumber);
+      const updatedCompanies = markPopularCompanies(data, popularIds);
+      setResults(updatedCompanies);
     } catch (error) {
       console.error('Error fetching top companies:', error);
       setResults([]);
@@ -76,7 +98,7 @@ const Home: React.FC = () => {
 
   return (
     <SplitPage sidebar={<Sidebar />}>
-      <SearchForm onSearch={fetchCompanies} />
+      <SearchForm onSearch={fetchCompanies} placeholder='企業名を入力(空白で全件表示)' />
       <Content companies={results} /> {/* 検索結果をContentに渡す */}
     </SplitPage>
   );
