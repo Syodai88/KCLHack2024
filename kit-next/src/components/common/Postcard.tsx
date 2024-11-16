d977859fdd057c549e974c4b68e0156d04c5c5eb
Syodai88 <syodi0808@gmail.com>
2024-11-16 15:51:08 +0900

    Post削除時の処理に確認Modalを追加

--- kit-next/src/components/common/Postcard.tsx
+++ kit-next/src/components/common/Postcard.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import { FaHeart, FaRegHeart, FaTrash } from 'react-icons/fa';
 import styles from './Postcard.module.css';
 import axios from 'axios';
+import ConfirmModal from './ConfirmModal';
+import Loading from './Loading';
 
 interface Tag {
     id: number;
@@ -37,6 +39,8 @@ const PostCard: React.FC<PostCardProps> = ({ post, loginUserId, isLiked }) => {
   const router = useRouter();
   const [isLikeState, setIsLikeState] = useState<boolean>(isLiked);
   const [currentLikeCount, setCurrentLikeCount] = useState<number>(post.likeCount);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleCardClick = () => {
     router.push(`/posts/${post.id}`);
@@ -45,10 +49,9 @@ const PostCard: React.FC<PostCardProps> = ({ post, loginUserId, isLiked }) => {
   const handleLikeClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
     try {
-        // いいね API の呼び出し
         const response = await axios.post('/api/postReaction', {
           postId: post.id,
-          userId: loginUserId, // ログイン中のユーザーのIDを渡す
+          userId: loginUserId, 
         });
   
         if (response.data.likeAdded) {
@@ -63,75 +66,105 @@ const PostCard: React.FC<PostCardProps> = ({ post, loginUserId, isLiked }) => {
       }
   };
 
-  const handleDeletePost = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    if (confirm('この投稿を削除しますか？')) {
-      try {
-        const response = await axios.delete('/api/deleteContent',{
-          params: {
-            contentId: post.id,
-            userId: loginUserId,
-            type: "post",
-          },
-        });
-        if (response.status === 200) {
-          alert('投稿を削除しました。');
-          router.refresh();
-        } else {
-          alert('投稿の削除に失敗しました。');
-        }
-      } catch (error) {
-        console.error('投稿の削除に失敗しました:', error);
-        alert('投稿の削除に失敗しました。');
+  const handleDeletePost = async () => {
+    try {
+      const response = await axios.delete('/api/deleteContent',{
+        params: {
+          contentId: post.id,
+          userId: loginUserId,
+          type: "post",
+        },
+      });
+      if (response.status === 200) {
+        router.refresh();
+      } else {
+        setIsLoading(true);
+        setTimeout(() => {
+          setIsLoading(false);
+      }, 3000);
       }
+    } catch (error) {
+      console.error('投稿の削除に失敗しました:', error);
+      setIsLoading(true);
+        setTimeout(() => {
+          setIsLoading(false);
+      }, 3000);
     }
+  };
+
+  const handleDeleteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsModalOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    handleDeletePost();
+    setIsModalOpen(false);
+  };
+
+  const handleCancelDelete = () => {
+    setIsModalOpen(false);
   };
 
   const contentSnippet = post.content.slice(0, 100); // 最初の100文字を表示
   const isContentTruncated = post.content.length > 100;
 
   return (
-    <Card className={styles.card} onClick={handleCardClick}>
-      <CardContent onClick={(e) => e.stopPropagation()}>
-        <Typography variant="h5" component="div" className={styles.title}>
-          {post.title}
-        </Typography>
-        <Typography variant="subtitle1" color="text.primary" className={styles.subtitle}>
-          企業: {post.company.name}
-        </Typography>
-        <Typography variant="subtitle1" color="text.primary" className={styles.subtitle}>
-          投稿者: {post.user.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" className={styles.postDate}>
-          投稿日: {new Date(post.createdAt).toLocaleDateString()}
-        </Typography>
-        <Typography variant="body1" color="text.primary" className={styles.content}>
-          {contentSnippet}
-          {isContentTruncated && '...'}
-        </Typography>
-        {isContentTruncated && (
-          <Typography variant="body2" color="primary" className={styles.readMore} onClick={handleCardClick}>
-            続きを読む
+    <>
+      <Card className={styles.card} onClick={handleCardClick}>
+        <CardContent onClick={(e) => e.stopPropagation()}>
+          <Typography variant="h5" component="div" className={styles.title}>
+            {post.title}
           </Typography>
-        )}
-      </CardContent>
-      <div className={styles.actionButtons}>
-        <div className={styles.reactionButtons}>
-          <button onClick={handleLikeClick} className={`${styles.button} ${isLikeState ? styles.liked : ''}`}>
-            {isLikeState ? <FaHeart /> : <FaRegHeart />}
-            いいね {currentLikeCount}
-          </button>
-          <button onClick={handleCardClick} className={styles.detailButton}>
-            詳細を見る
-          </button>
-          {loginUserId === post.userId && (
-            <button onClick={handleDeletePost} className={styles.deleteButton}>
-              <FaTrash /> 削除
-            </button>
+          <Typography variant="subtitle1" color="text.primary" className={styles.subtitle}>
+            企業: {post.company.name}
+          </Typography>
+          <Typography variant="subtitle1" color="text.primary" className={styles.subtitle}>
+            投稿者: {post.user.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" className={styles.postDate}>
+            投稿日: {new Date(post.createdAt).toLocaleDateString()}
+          </Typography>
+          <Typography variant="body1" color="text.primary" className={styles.content}>
+            {contentSnippet}
+            {isContentTruncated && '...'}
+          </Typography>
+          {isContentTruncated && (
+            <Typography variant="body2" color="primary" className={styles.readMore} onClick={handleCardClick}>
+              続きを読む
+            </Typography>
           )}
+        </CardContent>
+        <div className={styles.actionButtons}>
+          <div className={styles.reactionButtons}>
+            <button onClick={handleLikeClick} className={`${styles.button} ${isLikeState ? styles.liked : ''}`}>
+              {isLikeState ? <FaHeart /> : <FaRegHeart />}
+              いいね {currentLikeCount}
+            </button>
+            <button onClick={handleCardClick} className={styles.detailButton}>
+              詳細を見る
+            </button>
+            {loginUserId === post.userId && (
+              <button onClick={handleDeleteClick} className={styles.deleteButton}>
+                <FaTrash /> 削除
+              </button>
+            )}
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+      <ConfirmModal
+        isOpen={isModalOpen}
+        message="この投稿を削除しますか？"
+        type="confirm"
+        onConfirm={handleConfirmDelete}
+        onCancel={handleCancelDelete}
+        confirmText="削除"
+        cancelText="キャンセル"
+      />
+      {isLoading && (
+        <Loading type='Error' message='Error' />
+      )}
+    </>
   );
 };
 
